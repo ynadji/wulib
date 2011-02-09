1815ad11237037df967c810327e9238353e7f10a
Yacin Nadji <yacin@gatech.edu>
2011-02-09 00:32:55 +0000

    Added "unique"

--- wulib.py
+++ wulib.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
 #
 
+from itertools import ifilterfalse
+
 def chunks(l, n):
     """ Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i+n]
+
+# Helpful "itertools" functions
+# see: http://docs.python.org/library/itertools.html
+def unique(iterable, key=None):
+    """List unique elements, preserving order. Remember all elements ever seen.
+
+    unique_everseen('AAAABBBCCDAABBB') --> A B C D
+    unique_everseen('ABBCcAD', str.lower) --> A B C D"""
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
