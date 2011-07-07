77c8790a08ee576639e953725802e18305231c7c
Yacin Nadji <yacin@gatech.edu>
2011-07-07 14:55:56 -0400

    IPList() is an IP/CIDR black/white/graylist

--- wulib.py
+++ wulib.py
@@ -215,3 +215,57 @@ class DomainList(object):
         # If node == {}, the previous key was seen and is a leaf node.
         # Therefore, it's in the domain list.
         return node == {}
+
+class IPList(object):
+    """IP/CIDR whitelist/blacklist/greylist. A sorted list of networks is
+    created from the provided networks and a binary search is performed to see
+    if the provided IP exists in the list of networks. For all networks in the
+    list, none must be a subset of any other networks in the list. See the
+    comment in __init__() for a further explanation."""
+
+    from IPy import IP
+
+    def __init__(self, iplistfile):
+        self.networks = []
+        with open(iplistfile) as f:
+            for line in f:
+                self.networks.append(self.IP(line.strip()))
+
+        self.networks = sorted(self.networks, key=lambda x: x.int())
+        for i, net1 in enumerate(self.networks):
+            for net2 in self.networks[i+1:]:
+                try:
+                    # Networks can overlap in a way that breaks the correctness
+                    # of how I compute __contains__. For example:
+                    # >>> bogons = IPList('somefile')
+                    # >>> bogons.networks = sorted([IP('41.0.0.0/8'), IP('41.41.0.0/24'), IP('41.255.0.0/16')], key=lambda x: x.int())
+                    # >>> '41.100.0.0' in bogons
+                    # False
+                    #
+                    # This is obviously incorrect, and is due to the fact that
+                    # the middle network is contained within the first network.
+                    # When the provided IP is "larger" than the middle network,
+                    # it incorrectly chooses the upper half as the next step in
+                    # the recursion. To prevent this from happening, we throw a
+                    # ValueError if this condition exists in the list. For what
+                    # I'm doing right now, this isn't a problem but it may cause
+                    # problems in the future.
+                    assert(net1[-1].int() <= net2[-1].int())
+                except AssertionError:
+                    raise ValueError('%s is contained within %s!' % (net2, net1))
+
+    def _find(self, networks, ip):
+        mid = len(networks) / 2
+        try:
+            net = networks[mid]
+        except IndexError: # Not in list
+            return False
+        if ip in net:
+            return net
+        elif self.IP(ip).int() > net.int():
+            return self._find(networks[mid+1:], ip)
+        else:
+            return self._find(networks[:mid], ip)
+
+    def __contains__(self, ip):
+        return self._find(self.networks, ip)
