8c96383dbaab17a62c4bb3079ac582223a95da13
Yacin Nadji <yacin.nadji@corelight.com>
2020-02-05 11:38:40 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='wulib',
+    version='0.0.1',
+    description='A useful module',
+    author='Yacin Nadji',
+    author_email='ynadji@gmail.com',
+    packages=['wulib'],
+    install_requires=['IPy'],
+)
